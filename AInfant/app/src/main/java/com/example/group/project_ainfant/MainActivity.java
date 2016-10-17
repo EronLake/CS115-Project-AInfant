bddf651c6af6885b8ed90762383562fc910e1782
Michael Ha <mmha@ucsc.edu>
2016-10-17 12:30:03 -0700

    Database added

--- AInfant/app/src/main/java/com/example/group/project_ainfant/MainActivity.java
+++ AInfant/app/src/main/java/com/example/group/project_ainfant/MainActivity.java
@@ -1,23 +1,85 @@
 package com.example.group.project_ainfant;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.BaseColumns;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    DatabaseHelper myDb;
+    EditText input;
+    Button buttonAddData, buttonViewAll;
+
 
     /** called when activity is first created */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        input = (EditText)findViewById(R.id.inputText);
+        buttonAddData = (Button)findViewById(R.id.button_add);
+        buttonViewAll = (Button) findViewById(R.id.view_all);
+        addData();
+        viewAll();
+    }
+
+    public void addData() {
+        buttonAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(input.getText().toString());
+                        if(isInserted =true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void viewAll() {
+        buttonViewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb.getAllData();
+                        if(res.getCount() == 0) {
+                            // show message
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("Id :"+ res.getString(0)+"\n");
+                            buffer.append("Input :"+ res.getString(1)+"\n");
+                        }
+
+                        // Show all data
+                        showMessage("Data",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 
     @Override
@@ -42,7 +104,7 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private EditText input;
+
     private TextView output;
 
     public void buttonOnClick(View v){
