fa6f206c486f946b9a2282cb2c4d76eeb88ec091
Ricardo Munoz <riamunoz@ucsc.edu>
2016-11-05 14:17:31 -0700

    constructObject method changed

--- AInfant/app/src/main/java/com/example/group/project_ainfant/PartsOfSpeech/Word.java
+++ AInfant/app/src/main/java/com/example/group/project_ainfant/PartsOfSpeech/Word.java
@@ -11,17 +11,19 @@ public class Word {
     public String name;
     public String definition;
 
-}
 
-public Word constructObject(String word, String PoS, Hashtable<String , Integer> Tags){
-    if (PoS.equals("Noun")){
-        Noun new_Word = new Noun(Tags.get("type"), Tags.get("propVImp"), Tags.get("plurVSing"));
-    } else if (PoS.equals("Verb")){
 
+    public Word constructObject(String word, String PoS, Hashtable<String, Integer> Tags) {
+        if (PoS.equals("Noun")) {
+            Noun new_Word = new Noun(Tags.get("type"), Tags.get("propVImp"), Tags.get("plurVSing"));
+        } else if (PoS.equals("Verb")) {
+            //Verb new_Word =
+        } else if (PoS.equals("Verb")) {
+            //Verb new_Word =
+        }
+        return new_Word;
     }
-
 }
-
 
 /*
     still need to create preposition and conjunction classes
