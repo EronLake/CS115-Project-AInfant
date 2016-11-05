074a5bdaaa38538996da7a0193902c7795ad9be1
Alyx <alyxrjames@gmail.com>
2016-11-05 00:49:42 -0700

    Updated ParseTree

--- AInfant/app/src/main/java/com/example/group/project_ainfant/ParseTree.java
+++ AInfant/app/src/main/java/com/example/group/project_ainfant/ParseTree.java
@@ -7,37 +7,78 @@ import java.util.List;
  * Created by Alyx on 10/31/2016.
  */
 
+/*note as the Look Ahead goes the the list of objects, create a new Node for each element in the
+List and then attempts to shift reduce and build the tree up.
+*/
+
+
+/*The Look Ahead goes over the list of objects and attempts to shift-reduce the entire list the shift
+ reduced objects then are traced again by the parser and shift-reduces again. This process keeps
+ occurring until shift is able to reduce to a single node called sentence or fails to make a single
+ shift during an iteration in which case the sentence is invalid*/
+
+/*Root continuously adopts each node into itself until a shift reduce is possible*/
 public class ParseTree {
 
+    private Node root;
+    private List<Node> SRList;
 
-
-    ParseTree(Object o){
-
-        Node root = new Node(o);
+    public ParseTree(){
 
     }
-    public void AddChild(Node n){
+    //creates a node from an object
+    public Node newNode(Object o){
+        Node n = new Node(o);
+        return n;
+    }
+    //root adopts an object as its child node
+    public void adoptToRoot(Object o){
+        this.root.addNode(o);
+    }
+    //takes the root and object to shift reduce to and makes the SR object take the place of the
+    //root node and make the root the parent of the new SR node and then empties the root and adds
+    //the SR node to the tree
+    public void shiftReduce(Object o){
 
-       // addNode(n);
+        Node sr = newNode(o);
+        sr = root;
+        root.empty();
+        root.addNode(sr);
+        SRList.add(sr);
+
+    }
+    //gets the SRList
+    public List<Node> getSRList(){
+        return this.SRList;
+    }
+    //empties the SRList
+    public void clearSRList(){
+        this.SRList.clear();
+    }
+    //Node class, child is a list of nodes and each node holds an object
+    private static class Node{
+
+        private List<Node> child;
+        private Object obj;
+
+        private Node(Object o){
+
+            this.child = new ArrayList<Node>();
+            this.obj = o;
+
+        }
+        public void addNode(Object o){
+
+            this.child.add(new Node(o));
+
+        }
+        public void empty(){
+
+            this.child.clear();
+
+        }
 
     }
 
 }
-class Node{
 
-    private List<Node> child = null;
-    private Object obj;
-
-    public Node(Object o){
-
-        this.child = new ArrayList<>();
-        this.obj = o;
-
-    }
-    public void addNode(Node node){
-
-        this.child.add(node);
-
-    }
-
-}
