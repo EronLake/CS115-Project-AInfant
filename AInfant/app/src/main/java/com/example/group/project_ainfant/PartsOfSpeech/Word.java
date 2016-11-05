3a8e89b83de2b08e4dcd5887cbcb8d04ab5c6780
EronLake <ejlake@ucsc.edu>
2016-11-05 14:41:48 -0700

    fixed word error

--- AInfant/app/src/main/java/com/example/group/project_ainfant/PartsOfSpeech/Word.java
+++ AInfant/app/src/main/java/com/example/group/project_ainfant/PartsOfSpeech/Word.java
@@ -16,12 +16,21 @@ public class Word {
     public Word constructObject(String word, String PoS, Hashtable<String, Integer> Tags) {
         if (PoS.equals("Noun")) {
             Noun new_Word = new Noun(word, Tags.get("type"), Tags.get("propVImp"), Tags.get("plurVSing"));
+            return new_Word;
+
+        //Still needs to be filled out I just put the Noun constructor there to stop the error
         } else if (PoS.equals("Verb")) {
             //Verb new_Word =
+            Noun new_Word = new Noun(word, Tags.get("type"), Tags.get("propVImp"), Tags.get("plurVSing"));
+            return new_Word;
         } else if (PoS.equals("Verb")) {
             //Verb new_Word =
+            Noun new_Word = new Noun(word, Tags.get("type"), Tags.get("propVImp"), Tags.get("plurVSing"));
+            return new_Word;
+        } else{
+            Noun new_Word = new Noun(word, Tags.get("type"), Tags.get("propVImp"), Tags.get("plurVSing"));
+            return new_Word;
         }
-        return new_Word;
     }
 }
 
