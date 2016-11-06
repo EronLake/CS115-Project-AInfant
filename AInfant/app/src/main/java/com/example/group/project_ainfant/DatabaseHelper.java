fbf19b821bb5b4d726ac5b75f8677245859ef03e
Michael Ha <mmha@ucsc.edu>
2016-11-05 22:28:08 -0700

    this commit is for you robert

--- AInfant/app/src/main/java/com/example/group/project_ainfant/DatabaseHelper.java
+++ AInfant/app/src/main/java/com/example/group/project_ainfant/DatabaseHelper.java
@@ -15,7 +15,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "MyDBName.db";
     public static final String TABLE_NAME = "input_table";
-    public static final String Table_Input = "INPUT";
     public DatabaseHelper(Context context){
         super(context, DATABASE_NAME , null, 1);
     }
@@ -32,6 +31,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         initializeNoun(db);
         initializePronoun(db);
         initializeVerb(db);
+        db.execSQL("create table input_table " + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "INPUT text,"
+                + "speech text)");
 
     }
 
@@ -228,7 +230,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         //putting the user input into the INPUT column
-        contentValues.put(Table_Input, input);
+        contentValues.put("INPUT", input);
         long result = db.insert(TABLE_NAME,null,contentValues);
         if(result == -1){
             return false;
@@ -238,7 +240,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     public boolean ifExists(String exists) {
-        String query = "Select * FROM " + TABLE_NAME + " WHERE " + Table_Input + " =  \"" + exists + "\"";
+        String query = "Select * FROM " + TABLE_NAME + " WHERE " + "INPUT" + " =  \"" + exists + "\"";
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(query, null);
         if(cursor.getCount() <= 0){
