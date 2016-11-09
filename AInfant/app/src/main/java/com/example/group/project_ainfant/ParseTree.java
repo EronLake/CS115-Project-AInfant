d908ff6b30a3299568e611d6d3adbb52af2f769a
Robert <alyxrjames@gmail.com>
2016-11-08 16:58:50 -0800

    Editted ParseTree Added SyntaxCheck

--- AInfant/app/src/main/java/com/example/group/project_ainfant/ParseTree.java
+++ AInfant/app/src/main/java/com/example/group/project_ainfant/ParseTree.java
@@ -1,4 +1,4 @@
-package main.java.com.example.group.project_ainfant;
+package com.example.group.project_ainfant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +20,9 @@ List and then attempts to shift reduce and build the tree up.
 /*Root ccontinuously adopts each node into itself until a shift reduce is possible*/
 public class ParseTree {
 
-    private Node root;
-    private List<Node> SRList;
+    private Node ptr;
+    public List<Node> SRList;
+    public List<Node> Temp;
 
     public ParseTree(){
 
@@ -31,29 +32,34 @@ public class ParseTree {
         Node n = new Node(o);
         return n;
     }
-    //root adopts an object as its child node
-    public void adoptToRoot(Object o){
-        this.root.addNode(o);
+    //as you Use the Look Ahead and go through the
+    public void adoptToPtr(Node n){
+        this.ptr.addNode(n);
     }
-    //takes the root and object to shift reduce to and makes the SR object take the place of the
-    //root node and make the root the parent of the new SR node and then empties the root and adds
-    //the SR node to the tree
-    public void shiftReduce(Object o){
 
-        Node sr = newNode(o);
-        sr = root;
-        root.empty();
-        root.addNode(sr);
-        SRList.add(sr);
+    //use the ptr to shift reduce.
+    public void shiftReduce(List<Node> nds,Node node){
+
+        Node srnode = newNode(node.getNodeDate());
+        for(Node n: nds){
+            srnode.addNode(n);
+        }
+        Temp.add(srnode);
 
     }
     //gets the SRList
     public List<Node> getSRList(){
         return this.SRList;
     }
-    //empties the SRList
-    public void clearSRList(){
-        this.SRList.clear();
+    //returns the temp list
+    public List<Node> getTempList(){ return this.Temp; }
+    //empties the temp list
+    public void clearTemp(){
+        this.Temp.clear();
+    }
+    //sets the SRList and updates it to the temp list before it is cleared
+    public void updateSRList(){
+        this.SRList = this.Temp;
     }
     //Node class, child is a list of nodes and each node holds an object
     private static class Node{
@@ -72,9 +78,17 @@ public class ParseTree {
             this.child.add(new Node(o));
 
         }
+        public void addNode(Node n){
+            this.child.add(n);
+        }
         public void empty(){
 
             this.child.clear();
+
+        }
+        public Object getNodeDate(){
+
+            return this.obj;
 
         }
 
